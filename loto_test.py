03227bd26af84f912571594057005d205d4268ed
karas1k <45075660+karas1k@users.noreply.github.com>
2019-05-17 15:02:54 +0300

    Недоделанный лото, доделать!

---
+++ loto_test.py
@@ -0,0 +1,50 @@
+import random
+
+def card_loto():
+
+    mas_list = []
+    mas = [[] * 5 for i in range(1, 91)]
+    per = []
+
+    for i in range(3):
+        while len(mas[i]) < 5:
+
+            rand = random.sample(range(1, 91), 1)
+
+            if rand not in per:
+                mas[i].extend(rand)
+                mas[i] = sorted(mas[i])
+                per.append(rand)
+
+        mas_list.extend(mas[i])
+
+    print(f'Карточка игрока:\n{mas_list[0:5]}\n'
+          f'{mas_list[5:10]}\n{mas_list[10::]}\n')
+    return(mas_list)
+
+player = card_loto()
+computer = card_loto()
+
+mas_boch = []
+while len(mas_boch) != 90:
+
+    boch = random.randint(1,90)
+
+    if boch not in mas_boch:
+        mas_boch.append(boch)
+        if boch in player:
+            player.remove(boch)
+            print(f'Игрок {player}')
+            print(f'Компутер {computer}')
+        if boch in computer:
+            computer.remove(boch)
+            print(f'Игрок {player}')
+            print(f'Компутер {computer}')
+
+        if len(player) == 0:
+            print("Победил игрок!")
+            break
+
+        if len(computer) == 0:
+            print("Победил компутер!")
+            break
